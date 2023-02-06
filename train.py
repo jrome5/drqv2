896ccc86b683619ab8ad52314eaf4fbbf0f10c46
jack <jack.rome@hotmail.co.uk>
2023-02-06 17:49:29 +0000

    Remove comment lines and add safety net for unity call

--- train.py
+++ train.py
@@ -38,227 +38,225 @@ def make_agent(obs_spec, action_spec, cfg):
 def make_unity_env():
 	channel = EngineConfigurationChannel()
 	currpath = (os.path.dirname(os.path.abspath(__file__)))
+	#attempt another worker
+	try:
+		unity_env = UnityEnvironment(file_name=os.path.join(currpath,"envs/Crawlerv2/Crawler.x86_64"), side_channels=[channel], worker_id=1)
+	except:
+		print("Worker ID 1 already running. Attempting ID 2")
+		unity_env = UnityEnvironment(file_name=os.path.join(currpath,"envs/Crawlerv2/Crawler.x86_64"), side_channels=[channel], worker_id=2)
 
-	unity_env = UnityEnvironment(file_name=os.path.join(currpath,"envs/Crawlerv2/Crawler.x86_64"), side_channels=[channel], worker_id=2)
-
-	# unity_env = UnityEnvironment(file_name=os.path.join(currpath,"envs/ClothFold/ClothFold.x86_64"), side_channels=[channel], worker_id=2)
 	channel.set_configuration_parameters(time_scale = 5)
-
-	# unity_env = UnityEnvironment(file_name=, seed=1, side_channels=[], worker_id=2)
 	unity_env.reset()
 	env = UnityToGymWrapper(unity_env, uint8_visual=True)
-	#TODO: fix shape stack
 	observation_spec = specs.BoundedArray(shape=(9,84,84), dtype=np.uint8, name='observation', minimum=0, maximum=255)
-
-	# observation_spec = specs.BoundedArray(shape=np.insert(env.observation_space.shape, 0, 1), dtype=env.observation_space.dtype, minimum=0., maximum=1.)
 	action_spec = specs.BoundedArray(shape=env.action_space.shape, dtype=env.action_space.dtype, name='action', minimum =env.action_space.low, maximum=env.action_space.high)
 
 	return env, observation_spec, action_spec
+
 def un_normalize(obs):
 	return np.asarray(obs).transpose(2,1,0)
-	return obs#u_obs
-
+	return obs
 
 class Workspace:
-    def __init__(self, cfg):
-        self.work_dir = Path.cwd()
-        print(f'workspace: {self.work_dir}')
+	def __init__(self, cfg):
+		self.work_dir = Path.cwd()
+		print(f'workspace: {self.work_dir}')
 
-        self.cfg = cfg
-        utils.set_seed_everywhere(cfg.seed)
-        self.device = torch.device(cfg.device)
+		self.cfg = cfg
+		utils.set_seed_everywhere(cfg.seed)
+		self.device = torch.device(cfg.device)
 
-        self.observation_spec = None
-        self.action_spec = None
-        self.setup()
+		self.observation_spec = None
+		self.action_spec = None
+		self.setup()
 
-        self.agent = make_agent(self.observation_spec,
-                                self.action_spec,
-                                self.cfg.agent)
-        self.timer = utils.Timer()
-        self._global_step = 0
-        self._global_episode = 0
+		self.agent = make_agent(self.observation_spec,
+								self.action_spec,
+								self.cfg.agent)
+		self.timer = utils.Timer()
+		self._global_step = 0
+		self._global_episode = 0
 
-    def setup(self):
-        # create logger
-        self.logger = Logger(self.work_dir, use_tb=self.cfg.use_tb)
-        # create envs
-        self.train_env, self.observation_spec, self.action_spec = make_unity_env()
-        # create replay buffer
-        data_specs = (self.observation_spec,
-                        self.action_spec,
-                        specs.Array((1,), np.float32, 'reward'),
-                        specs.Array((1,), np.float32, 'discount'))
+	def setup(self):
+		# create logger
+		self.logger = Logger(self.work_dir, use_tb=self.cfg.use_tb)
+		# create envs
+		self.train_env, self.observation_spec, self.action_spec = make_unity_env()
+		# create replay buffer
+		data_specs = (self.observation_spec,
+						self.action_spec,
+						specs.Array((1,), np.float32, 'reward'),
+						specs.Array((1,), np.float32, 'discount'))
 
-        self.replay_storage = ReplayBufferStorage(data_specs,
-                                                  self.work_dir / 'buffer')
+		self.replay_storage = ReplayBufferStorage(data_specs,
+												  self.work_dir / 'buffer')
 
-        self.replay_loader = make_replay_loader(
-            self.work_dir / 'buffer', self.cfg.replay_buffer_size,
-            self.cfg.batch_size, self.cfg.replay_buffer_num_workers,
-            self.cfg.save_snapshot, self.cfg.nstep, self.cfg.discount)
-        self._replay_iter = None
+		self.replay_loader = make_replay_loader(
+			self.work_dir / 'buffer', self.cfg.replay_buffer_size,
+			self.cfg.batch_size, self.cfg.replay_buffer_num_workers,
+			self.cfg.save_snapshot, self.cfg.nstep, self.cfg.discount)
+		self._replay_iter = None
 
-        self.video_recorder = TrainVideoRecorder(
-            self.work_dir if self.cfg.save_video else None)
-        self.train_video_recorder = TrainVideoRecorder(
-            self.work_dir if self.cfg.save_train_video else None)
+		self.video_recorder = TrainVideoRecorder(
+			self.work_dir if self.cfg.save_video else None)
+		self.train_video_recorder = TrainVideoRecorder(
+			self.work_dir if self.cfg.save_train_video else None)
 
 
-    @property
-    def global_step(self):
-        return self._global_step
+	@property
+	def global_step(self):
+		return self._global_step
 
-    @property
-    def global_episode(self):
-        return self._global_episode
+	@property
+	def global_episode(self):
+		return self._global_episode
 
-    @property
-    def global_frame(self):
-        return self.global_step * self.cfg.action_repeat
+	@property
+	def global_frame(self):
+		return self.global_step * self.cfg.action_repeat
 
-    @property
-    def replay_iter(self):
-        if self._replay_iter is None:
-            self._replay_iter = iter(self.replay_loader)
-        return self._replay_iter
+	@property
+	def replay_iter(self):
+		if self._replay_iter is None:
+			self._replay_iter = iter(self.replay_loader)
+		return self._replay_iter
 
-    def eval(self):
-        step, episode, total_reward = 0, 0, 0
-        eval_until_episode = utils.Until(self.cfg.num_eval_episodes)
-        observation = None
+	def eval(self):
+		step, episode, total_reward = 0, 0, 0
+		eval_until_episode = utils.Until(self.cfg.num_eval_episodes)
+		observation = None
 
-        while eval_until_episode(episode):
-            observation = un_normalize(self.train_env.reset())
-            done = False
-            self.video_recorder.init(observation, enabled=(episode == 0))
-            while not done:
-                with torch.no_grad(), utils.eval_mode(self.agent):
-                    action = self.agent.act(observation,
-                                            self.global_step,
-                                            eval_mode=True)
-                n_observation, reward, done, info = self.train_env.step(action)
-                observation = un_normalize(n_observation)
-                self.video_recorder.record(observation)
-                total_reward += reward
-                step += 1
+		while eval_until_episode(episode):
+			observation = un_normalize(self.train_env.reset())
+			done = False
+			self.video_recorder.init(observation, enabled=(episode == 0))
+			while not done:
+				with torch.no_grad(), utils.eval_mode(self.agent):
+					action = self.agent.act(observation,
+											self.global_step,
+											eval_mode=True)
+				n_observation, reward, done, info = self.train_env.step(action)
+				observation = un_normalize(n_observation)
+				self.video_recorder.record(observation)
+				total_reward += reward
+				step += 1
 
-            episode += 1
-            self.video_recorder.save(f'{self.global_frame}.mp4')
+			episode += 1
+			self.video_recorder.save(f'{self.global_frame}.mp4')
 
-        with self.logger.log_and_dump_ctx(self.global_frame, ty='eval') as log:
-            log('episode_reward', total_reward / episode)
-            log('episode_length', step * self.cfg.action_repeat / episode)
-            log('episode', self.global_episode)
-            log('step', self.global_step)
+		with self.logger.log_and_dump_ctx(self.global_frame, ty='eval') as log:
+			log('episode_reward', total_reward / episode)
+			log('episode_length', step * self.cfg.action_repeat / episode)
+			log('episode', self.global_episode)
+			log('step', self.global_step)
 
-    def train(self):
-        # predicates
-        train_until_step = utils.Until(self.cfg.num_train_frames,
-                                        self.cfg.action_repeat)
-        seed_until_step = utils.Until(self.cfg.num_seed_frames,
-                                        self.cfg.action_repeat)
-        eval_every_step = utils.Every(self.cfg.eval_every_frames,
-                                        self.cfg.action_repeat)
+	def train(self):
+		# predicates
+		train_until_step = utils.Until(self.cfg.num_train_frames,
+										self.cfg.action_repeat)
+		seed_until_step = utils.Until(self.cfg.num_seed_frames,
+										self.cfg.action_repeat)
+		eval_every_step = utils.Every(self.cfg.eval_every_frames,
+										self.cfg.action_repeat)
 
-        episode_step, episode_reward = 0, 0
-        observation = un_normalize(self.train_env.reset())
-        action = np.zeros(self.action_spec.shape, dtype=self.action_spec.dtype)
-        discount = self.cfg.discount
-        self.replay_storage.add(observation, action, discount, 0.0, False)
-        self.train_video_recorder.init(observation)
-        metrics = None
-        done = False
-        while train_until_step(self.global_step):
-            if done:
-                self._global_episode += 1
-                self.train_video_recorder.save(f'{self.global_frame}.mp4')
-                # wait until all the metrics schema is populated
-                if metrics is not None:
-                    # log stats
-                    elapsed_time, total_time = self.timer.reset()
-                    episode_frame = episode_step * self.cfg.action_repeat
-                    with self.logger.log_and_dump_ctx(self.global_frame,
-                                                      ty='train') as log:
-                        log('fps', episode_frame / elapsed_time)
-                        log('total_time', total_time)
-                        log('episode_reward', episode_reward)
-                        log('episode_length', episode_frame)
-                        log('episode', self.global_episode)
-                        log('buffer_size', len(self.replay_storage))
-                        log('step', self.global_step)
+		episode_step, episode_reward = 0, 0
+		observation = un_normalize(self.train_env.reset())
+		action = np.zeros(self.action_spec.shape, dtype=self.action_spec.dtype)
+		discount = self.cfg.discount
+		self.replay_storage.add(observation, action, discount, 0.0, False)
+		self.train_video_recorder.init(observation)
+		metrics = None
+		done = False
+		while train_until_step(self.global_step):
+			if done:
+				self._global_episode += 1
+				self.train_video_recorder.save(f'{self.global_frame}.mp4')
+				# wait until all the metrics schema is populated
+				if metrics is not None:
+					# log stats
+					elapsed_time, total_time = self.timer.reset()
+					episode_frame = episode_step * self.cfg.action_repeat
+					with self.logger.log_and_dump_ctx(self.global_frame,
+													  ty='train') as log:
+						log('fps', episode_frame / elapsed_time)
+						log('total_time', total_time)
+						log('episode_reward', episode_reward)
+						log('episode_length', episode_frame)
+						log('episode', self.global_episode)
+						log('buffer_size', len(self.replay_storage))
+						log('step', self.global_step)
 
-                # reset env
-                observation = un_normalize(self.train_env.reset())
-                action = np.zeros(self.action_spec.shape, dtype=self.action_spec.dtype)
-                reward = 0.0
-                done = False
-                self.replay_storage.add(observation, action, discount, reward, done)
-                self.train_video_recorder.init(observation)
-                # try to save snapshot
-                if self.cfg.save_snapshot and self._global_episode % self.cfg.save_snapshot_every == 0:
-                    self.save_snapshot()
-                episode_step = 0
-                episode_reward = 0
+				# reset env
+				observation = un_normalize(self.train_env.reset())
+				action = np.zeros(self.action_spec.shape, dtype=self.action_spec.dtype)
+				reward = 0.0
+				done = False
+				self.replay_storage.add(observation, action, discount, reward, done)
+				self.train_video_recorder.init(observation)
+				# try to save snapshot
+				if self.cfg.save_snapshot and self._global_episode % self.cfg.save_snapshot_every == 0:
+					self.save_snapshot()
+				episode_step = 0
+				episode_reward = 0
 
-            # try to evaluate
-            if eval_every_step(self.global_step):
-                self.logger.log('eval_total_time', self.timer.total_time(),
-                                self.global_frame)
-                self.eval()
-                observation = un_normalize(self.train_env.reset())
-                action = np.zeros(self.action_spec.shape, dtype=self.action_spec.dtype)
-                reward = 0.0
-                done = False
+			# try to evaluate
+			if eval_every_step(self.global_step):
+				self.logger.log('eval_total_time', self.timer.total_time(),
+								self.global_frame)
+				self.eval()
+				observation = un_normalize(self.train_env.reset())
+				action = np.zeros(self.action_spec.shape, dtype=self.action_spec.dtype)
+				reward = 0.0
+				done = False
 
-            # sample action
-            with torch.no_grad(), utils.eval_mode(self.agent):
-                action = self.agent.act(observation,
-                                        self.global_step,
-                                        eval_mode=False)
+			# sample action
+			with torch.no_grad(), utils.eval_mode(self.agent):
+				action = self.agent.act(observation,
+										self.global_step,
+										eval_mode=False)
 
-            # try to update the agent
-            if not seed_until_step(self.global_step):
-                metrics = self.agent.update(self.replay_iter, self.global_step)
-                self.logger.log_metrics(metrics, self.global_frame, ty='train')
+			# try to update the agent
+			if not seed_until_step(self.global_step):
+				metrics = self.agent.update(self.replay_iter, self.global_step)
+				self.logger.log_metrics(metrics, self.global_frame, ty='train')
 
-            # take env step
-            n_observation, reward, done, info = self.train_env.step(action)
-            observation = un_normalize(n_observation)
+			# take env step
+			n_observation, reward, done, info = self.train_env.step(action)
+			observation = un_normalize(n_observation)
 
-            episode_reward += reward
-            self.replay_storage.add(observation, action, discount, reward, done)
-            self.train_video_recorder.record(observation)
-            episode_step += 1
-            self._global_step += 1
+			episode_reward += reward
+			self.replay_storage.add(observation, action, discount, reward, done)
+			self.train_video_recorder.record(observation)
+			episode_step += 1
+			self._global_step += 1
 
-    def save_snapshot(self):
-        snapshot = self.work_dir / 'snapshot.pt'
-        keys_to_save = ['agent', 'timer', '_global_step', '_global_episode']
-        payload = {k: self.__dict__[k] for k in keys_to_save}
-        with snapshot.open('wb') as f:
-            torch.save(payload, f)
-        print(f'snapshot saved to {snapshot}')
+	def save_snapshot(self):
+		snapshot = self.work_dir / 'snapshot.pt'
+		keys_to_save = ['agent', 'timer', '_global_step', '_global_episode']
+		payload = {k: self.__dict__[k] for k in keys_to_save}
+		with snapshot.open('wb') as f:
+			torch.save(payload, f)
+		print(f'snapshot saved to {snapshot}')
 
-    def load_snapshot(self):
-        snapshot = self.work_dir / 'snapshot.pt'
-        with snapshot.open('rb') as f:
-            payload = torch.load(f)
-        for k, v in payload.items():
-            self.__dict__[k] = v
+	def load_snapshot(self):
+		snapshot = self.work_dir / 'snapshot.pt'
+		with snapshot.open('rb') as f:
+			payload = torch.load(f)
+		for k, v in payload.items():
+			self.__dict__[k] = v
 
 
 @hydra.main(config_path='cfgs', config_name='config')
 def main(cfg):
-    from train import Workspace as W
-    root_dir = Path.cwd()
-    workspace = W(cfg)
-    snapshot = root_dir / 'snapshot.pt'
-    if snapshot.exists():
-        print(f'resuming: {snapshot}')
-        workspace.load_snapshot()
-    workspace.train()
+	from train import Workspace as W
+	root_dir = Path.cwd()
+	workspace = W(cfg)
+	snapshot = root_dir / 'snapshot.pt'
+	if snapshot.exists():
+		print(f'resuming: {snapshot}')
+		workspace.load_snapshot()
+	workspace.train()
 
 
 if __name__ == '__main__':
-    main()
+	main()
