0dcb411022fd1902ce41dfee80f9c883b7d74081
jack <jack.rome@hotmail.co.uk>
2023-02-06 17:55:02 +0000

    Remove unneeded imports

--- train.py
+++ train.py
@@ -2,12 +2,7 @@
 #
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
-import warnings
-warnings.filterwarnings('ignore', category=DeprecationWarning)
-
 import os
-os.environ['MKL_SERVICE_FORCE_INTEL'] = '1'
-os.environ['MUJOCO_GL'] = 'egl'
 
 from pathlib import Path
 
